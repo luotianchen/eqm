4fbb31c332e133420cb3587f9a9b048adbb9aa04
石泳键 <931822897@qq.com>
2019-01-30 13:16:56 +0800

    新增 根据牌号查询材料标准（通过标准表查） 接口

--- src/start/searchmatlstandbydes/searchmatlstandbydes.java
+++ src/start/searchmatlstandbydes/searchmatlstandbydes.java
@@ -1,4 +1,44 @@
 package start.searchmatlstandbydes;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import start.jdbc.jdbc;
+
+import java.sql.*;
+import java.util.ArrayList;
+
+@Controller
+@CrossOrigin
 public class searchmatlstandbydes {                                             //根据牌号查询材料标准（通过标准表查）
+    @RequestMapping(value = "searchmatlstandbydes")
+    public @ResponseBody searchmatlstandbydesresult searchmatlstandbydes(@RequestBody searchmatlstandbydespost sp) throws ClassNotFoundException, SQLException {
+        jdbc j = new jdbc();
+        Class.forName(j.getDBDRIVER());
+        Connection conn = DriverManager.getConnection(j.getDBURL(),j.getDBUSER(),j.getDBPASS());
+        PreparedStatement ps = null;
+        ResultSet rs=null;
+
+        searchmatlstandbydesresult result = new searchmatlstandbydesresult();
+        ArrayList<String> as = new ArrayList<String>();
+
+        try {
+            ps = conn.prepareStatement("SELECT * FROM contraststand WHERE designation = ?");
+            ps.setString(1,sp.getDesignation());
+            rs = ps.executeQuery();
+            while (rs.next()){
+                as.add(rs.getString("matlstand"));
+            }
+            rs.close();
+            ps.close();
+            result.setResult("success");
+            result.setMatlstand(as);
+        }catch (Exception e){
+            result.setResult("fail");
+        }
+        conn.close();
+        return result;
+    }
 }
--- src/start/searchmatlstandbydes/searchmatlstandbydesresult.java
+++ src/start/searchmatlstandbydes/searchmatlstandbydesresult.java
@@ -1,4 +1,27 @@
 package start.searchmatlstandbydes;
 
+import java.util.ArrayList;
+
 public class searchmatlstandbydesresult {
+    private String result;
+    private ArrayList<String> matlstand;
+    public searchmatlstandbydesresult(){
+        super();
+    }
+
+    public String getResult() {
+        return result;
+    }
+
+    public void setResult(String result) {
+        this.result = result;
+    }
+
+    public ArrayList<String> getMatlstand() {
+        return matlstand;
+    }
+
+    public void setMatlstand(ArrayList<String> matlstand) {
+        this.matlstand = matlstand;
+    }
 }
