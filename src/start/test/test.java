6baf4e4da537a3b721d47c0303b9faa44054a58c
石泳键 <931822897@qq.com>
2019-03-11 18:17:24 +0800

    233

--- src/start/test/test.java
+++ src/start/test/test.java
@@ -5,6 +5,23 @@ import com.jacob.com.ComThread;
 import com.jacob.com.Dispatch;
 import com.jacob.com.Variant;
 import org.apache.commons.io.FileUtils;
+import org.apache.poi.hssf.usermodel.HSSFCell;
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.util.CellReference;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,27 +30,57 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 @CrossOrigin
 @Controller
 public class test {
     @RequestMapping(value = "test")
-    public @ResponseBody String test(@RequestParam(value="excel") MultipartFile multipartFile, HttpServletRequest request) throws IOException {
+    public @ResponseBody ResponseEntity<byte[]> test(@RequestParam(value="excel") MultipartFile multipartFile, HttpServletRequest request) throws Exception {
         String realPath = request.getSession().getServletContext().getRealPath("");
         InputStream inputStream = multipartFile.getInputStream();                           //服务器根目录的路径
         String path = realPath;                                                             //根目录下新建文件夹upload，存放上传图片
         String uploadPath = path + "upload";                                                //获取文件名称
         String filename = getUploadFileName(multipartFile);                                 //将文件上传的服务器根目录下的upload文件夹
         File file = new File(uploadPath, filename);
+
+
+
         FileUtils.copyInputStreamToFile(inputStream, file);
-        String url1 = request.getScheme() + "://" + request.getServerName() + ":" + request.getServerPort() + "/eqm/upload/" + filename;
-        String url2 = request.getScheme() + "://" + request.getServerName() + ":" + request.getServerPort() + "/eqm/upload/" + "压力容器产品质量计划.pdf";
-        return url2;
+        String url1 = uploadPath +"/"+ filename;
+        String url2 = uploadPath +"/"+ "123.pdf";
+        System.out.println(url1);
+        System.out.println(url2);
+
+
+
+
+        FileInputStream fileXlsx = new FileInputStream(url1);
+        XSSFWorkbook workBook = new XSSFWorkbook(fileXlsx);
+        fileXlsx.close();
+        Sheet sheet=workBook.getSheetAt(0);
+        List<List<String>> result = readXlsx(workBook);
+        System.out.println(result.get(8).get(11));
+        putsheet(sheet,8,11,"a");
+        OutputStream out = new FileOutputStream(url1);
+        workBook.write(out);
+        out.close();
+
+
+
+        excel2Pdf(url1,url2);
+        File filepdf = new File(uploadPath, "123.pdf");
+        HttpHeaders headers = new HttpHeaders();// 设置一个head
+        headers.setContentDispositionFormData("attachment", "123.pdf");// 文件的属性，也就是文件叫什么吧
+        headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);// 内容是字节流
+        ResponseEntity<byte[]> download = new ResponseEntity<byte[]>(FileUtils.readFileToByteArray(filepdf),headers, HttpStatus.CREATED);
+        file.delete();
+        filepdf.delete();
+        return download;
     }
 
     private String getUploadFileName(MultipartFile multipartFile) {
@@ -46,6 +93,7 @@ public class test {
         String name = fileName + "_" + timeStr + type;
         return name;
     }
+
 
     public static void excel2Pdf(String inFilePath, String outFilePath) throws Exception {
         ActiveXComponent ax = null;
@@ -93,5 +141,88 @@ public class test {
             }
             ComThread.Release();
         }
+    }
+
+
+    private List<List<String>> readXlsx(XSSFWorkbook xssfWorkbook) throws Exception {
+        List<List<String>> result = new ArrayList<List<String>>();
+        // 循环每一页，并处理当前循环页
+        for (XSSFSheet xssfSheet : xssfWorkbook) {
+            if (xssfSheet == null) {
+                continue;
+            }
+            // 处理当前页，循环读取每一行
+            for (int rowNum = 1; rowNum <= xssfSheet.getLastRowNum(); rowNum++) {
+                XSSFRow xssfRow = xssfSheet.getRow(rowNum);
+                int minColIx = xssfRow.getFirstCellNum();
+                int maxColIx = xssfRow.getLastCellNum();
+                List<String> rowList = new ArrayList<String>();
+                for (int colIx = minColIx; colIx < maxColIx; colIx++) {
+                    XSSFCell cell = xssfRow.getCell(colIx);
+                    if (cell == null) {
+                        continue;
+                    }
+                    rowList.add(cell.toString());
+                }
+                result.add(rowList);
+            }
+        }
+        return result;
+    }
+
+
+    private List<List<String>> readXls(HSSFWorkbook hssfWorkbook) throws Exception {
+        List<List<String>> result = new ArrayList<List<String>>();
+        int size = hssfWorkbook.getNumberOfSheets();
+        // 循环每一页，并处理当前循环页
+        for (int numSheet = 0; numSheet < size; numSheet++) {
+            // HSSFSheet 标识某一页
+            HSSFSheet hssfSheet = hssfWorkbook.getSheetAt(numSheet);
+            if (hssfSheet == null) {
+                continue;
+            }
+            // 处理当前页，循环读取每一行
+            for (int rowNum = 1; rowNum <= hssfSheet.getLastRowNum(); rowNum++) {
+                // HSSFRow表示行
+                HSSFRow hssfRow = hssfSheet.getRow(rowNum);
+                int minColIx = hssfRow.getFirstCellNum();
+                int maxColIx = hssfRow.getLastCellNum();
+                List<String> rowList = new ArrayList<String>();
+                // 遍历改行，获取处理每个cell元素
+                for (int colIx = minColIx; colIx < maxColIx; colIx++) {
+                    // HSSFCell 表示单元格
+                    HSSFCell cell = hssfRow.getCell(colIx);
+                    if (cell == null) {
+                        continue;
+                    }
+                    rowList.add(getStringVal(cell));
+                }
+                result.add(rowList);
+            }
+        }
+        return result;
+    }
+
+    public static String getStringVal(HSSFCell cell) {
+        switch (cell.getCellType()) {
+            case Cell.CELL_TYPE_BOOLEAN:
+                return cell.getBooleanCellValue() ? "TRUE" : "FALSE";
+            case Cell.CELL_TYPE_FORMULA:
+                return cell.getCellFormula();
+            case Cell.CELL_TYPE_NUMERIC:
+                cell.setCellType(Cell.CELL_TYPE_STRING);
+                return cell.getStringCellValue();
+            case Cell.CELL_TYPE_STRING:
+                return cell.getStringCellValue();
+            default:
+                return "";
+        }
+    }
+
+    private void putsheet(Sheet sheet,int rownum,int cellnum,String mes) throws IOException {
+        Row row = sheet.getRow(rownum);
+        Cell cell =row.getCell(cellnum);
+        CellReference cr = new CellReference(cell);
+        cell.setCellValue(mes);
     }
 }
