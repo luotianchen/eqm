10e6b2bfce426dc40b3356ef639a81565a50ea59
石泳键 <931822897@qq.com>
2019-05-01 17:14:16 +0800

    233

--- src/start/searchallmaterial/searchallmaterialdata.java
+++ src/start/searchallmaterial/searchallmaterialdata.java
@@ -19,44 +19,44 @@ public class searchallmaterialdata {
     private String dimension;                             //尺寸
     private String millunit;                            //生存单位*
     private String heatbatchno;                         //炉批号
-    private double c;
-    private double si;
-    private double mn;
-    private double cu;
-    private double ni;
-    private double cr;
-    private double mo;
-    private double nb;
-    private double v;
-    private double ti;
-    private double als;
-    private double alt;
-    private double n;
-    private double fe;
-    private double mg;
-    private double zn;
-    private double b;
-    private double w;
-    private double sb;
-    private double al;
-    private double zr;
-    private double ca;
-    private double be;
-    private double p;
-    private double s;
+    private String c;
+    private String si;
+    private String mn;
+    private String cu;
+    private String ni;
+    private String cr;
+    private String mo;
+    private String nb;
+    private String v;
+    private String ti;
+    private String als;
+    private String alt;
+    private String n;
+    private String fe;
+    private String mg;
+    private String zn;
+    private String b;
+    private String w;
+    private String sb;
+    private String al;
+    private String zr;
+    private String ca;
+    private String be;
+    private String p;
+    private String s;
     private String heatcondi;                        //热处理状态*
-    private int rel1;                                    //屈服强度
-    private int rel2;
-    private int rm1;                                     //抗拉强度
-    private int rm2;
-    private double elong1;                                  //延长率
-    private double elong2;
+    private String rel1;                                    //屈服强度
+    private String rel2;
+    private String rm1;                                     //抗拉强度
+    private String rm2;
+    private String elong1;                                  //延长率
+    private String elong2;
     private String hardness1;                               //硬度
     private String hardness2;
     private String hardness3;
-    private int impactp1;                                //冲击功
-    private int impactp2;
-    private int impactp3;
+    private String impactp1;                                //冲击功
+    private String impactp2;
+    private String impactp3;
     private String impacttemp;                              //冲击温度*
     private String bendangle;                               //弯曲角度*
     private String bendaxdia;                               //弯曲直径
@@ -64,280 +64,6 @@ public class searchallmaterialdata {
     public searchallmaterialdata(){
         super();
     }
-
-
-    public double getC() {
-        return c;
-    }
-
-    public void setC(double c) {
-        this.c = c;
-    }
-
-    public double getSi() {
-        return si;
-    }
-
-    public void setSi(double si) {
-        this.si = si;
-    }
-
-    public double getMn() {
-        return mn;
-    }
-
-    public void setMn(double mn) {
-        this.mn = mn;
-    }
-
-    public double getCu() {
-        return cu;
-    }
-
-    public void setCu(double cu) {
-        this.cu = cu;
-    }
-
-    public double getNi() {
-        return ni;
-    }
-
-    public void setNi(double ni) {
-        this.ni = ni;
-    }
-
-    public double getCr() {
-        return cr;
-    }
-
-    public void setCr(double cr) {
-        this.cr = cr;
-    }
-
-    public double getMo() {
-        return mo;
-    }
-
-    public void setMo(double mo) {
-        this.mo = mo;
-    }
-
-    public double getNb() {
-        return nb;
-    }
-
-    public void setNb(double nb) {
-        this.nb = nb;
-    }
-
-    public double getV() {
-        return v;
-    }
-
-    public void setV(double v) {
-        this.v = v;
-    }
-
-    public double getTi() {
-        return ti;
-    }
-
-    public void setTi(double ti) {
-        this.ti = ti;
-    }
-
-    public double getAls() {
-        return als;
-    }
-
-    public void setAls(double als) {
-        this.als = als;
-    }
-
-    public double getAlt() {
-        return alt;
-    }
-
-    public void setAlt(double alt) {
-        this.alt = alt;
-    }
-
-    public double getN() {
-        return n;
-    }
-
-    public void setN(double n) {
-        this.n = n;
-    }
-
-    public double getFe() {
-        return fe;
-    }
-
-    public void setFe(double fe) {
-        this.fe = fe;
-    }
-
-    public double getMg() {
-        return mg;
-    }
-
-    public void setMg(double mg) {
-        this.mg = mg;
-    }
-
-    public double getZn() {
-        return zn;
-    }
-
-    public void setZn(double zn) {
-        this.zn = zn;
-    }
-
-    public double getB() {
-        return b;
-    }
-
-    public void setB(double b) {
-        this.b = b;
-    }
-
-    public double getW() {
-        return w;
-    }
-
-    public void setW(double w) {
-        this.w = w;
-    }
-
-    public double getSb() {
-        return sb;
-    }
-
-    public void setSb(double sb) {
-        this.sb = sb;
-    }
-
-    public double getAl() {
-        return al;
-    }
-
-    public void setAl(double al) {
-        this.al = al;
-    }
-
-    public double getZr() {
-        return zr;
-    }
-
-    public void setZr(double zr) {
-        this.zr = zr;
-    }
-
-    public double getCa() {
-        return ca;
-    }
-
-    public void setCa(double ca) {
-        this.ca = ca;
-    }
-
-    public double getBe() {
-        return be;
-    }
-
-    public void setBe(double be) {
-        this.be = be;
-    }
-
-    public double getP() {
-        return p;
-    }
-
-    public void setP(double p) {
-        this.p = p;
-    }
-
-    public double getS() {
-        return s;
-    }
-
-    public void setS(double s) {
-        this.s = s;
-    }
-
-    public int getRel1() {
-        return rel1;
-    }
-
-    public void setRel1(int rel1) {
-        this.rel1 = rel1;
-    }
-
-    public int getRel2() {
-        return rel2;
-    }
-
-    public void setRel2(int rel2) {
-        this.rel2 = rel2;
-    }
-
-    public int getRm1() {
-        return rm1;
-    }
-
-    public void setRm1(int rm1) {
-        this.rm1 = rm1;
-    }
-
-    public int getRm2() {
-        return rm2;
-    }
-
-    public void setRm2(int rm2) {
-        this.rm2 = rm2;
-    }
-
-    public double getElong1() {
-        return elong1;
-    }
-
-    public void setElong1(double elong1) {
-        this.elong1 = elong1;
-    }
-
-    public double getElong2() {
-        return elong2;
-    }
-
-    public void setElong2(double elong2) {
-        this.elong2 = elong2;
-    }
-
-    public int getImpactp1() {
-        return impactp1;
-    }
-
-    public void setImpactp1(int impactp1) {
-        this.impactp1 = impactp1;
-    }
-
-    public int getImpactp2() {
-        return impactp2;
-    }
-
-    public void setImpactp2(int impactp2) {
-        this.impactp2 = impactp2;
-    }
-
-    public int getImpactp3() {
-        return impactp3;
-    }
-
-    public void setImpactp3(int impactp3) {
-        this.impactp3 = impactp3;
-    }
-
     public String getCodedmarking() {
         return codedmarking;
     }
@@ -467,6 +193,174 @@ public class searchallmaterialdata {
         this.heatbatchno = heatbatchno;
     }
 
+    public String getC() {
+        return c;
+    }
+
+    public void setC(String c) {
+        this.c = c;
+    }
+
+    public String getSi() {
+        return si;
+    }
+
+    public void setSi(String si) {
+        this.si = si;
+    }
+
+    public String getMn() {
+        return mn;
+    }
+
+    public void setMn(String mn) {
+        this.mn = mn;
+    }
+
+    public String getCu() {
+        return cu;
+    }
+
+    public void setCu(String cu) {
+        this.cu = cu;
+    }
+
+    public String getNi() {
+        return ni;
+    }
+
+    public void setNi(String ni) {
+        this.ni = ni;
+    }
+
+    public String getCr() {
+        return cr;
+    }
+
+    public void setCr(String cr) {
+        this.cr = cr;
+    }
+
+    public String getMo() {
+        return mo;
+    }
+
+    public void setMo(String mo) {
+        this.mo = mo;
+    }
+
+    public String getNb() {
+        return nb;
+    }
+
+    public void setNb(String nb) {
+        this.nb = nb;
+    }
+
+    public String getV() {
+        return v;
+    }
+
+    public void setV(String v) {
+        this.v = v;
+    }
+
+    public String getTi() {
+        return ti;
+    }
+
+    public void setTi(String ti) {
+        this.ti = ti;
+    }
+
+    public String getAlt() {
+        return alt;
+    }
+
+    public void setAlt(String alt) {
+        this.alt = alt;
+    }
+
+    public String getN() {
+        return n;
+    }
+
+    public void setN(String n) {
+        this.n = n;
+    }
+
+    public String getMg() {
+        return mg;
+    }
+
+    public void setMg(String mg) {
+        this.mg = mg;
+    }
+
+    public String getP() {
+        return p;
+    }
+
+    public void setP(String p) {
+        this.p = p;
+    }
+
+    public String getS() {
+        return s;
+    }
+
+    public void setS(String s) {
+        this.s = s;
+    }
+
+    public String getRel1() {
+        return rel1;
+    }
+
+    public void setRel1(String rel1) {
+        this.rel1 = rel1;
+    }
+
+    public String getRel2() {
+        return rel2;
+    }
+
+    public void setRel2(String rel2) {
+        this.rel2 = rel2;
+    }
+
+    public String getRm1() {
+        return rm1;
+    }
+
+    public void setRm1(String rm1) {
+        this.rm1 = rm1;
+    }
+
+    public String getRm2() {
+        return rm2;
+    }
+
+    public void setRm2(String rm2) {
+        this.rm2 = rm2;
+    }
+
+    public String getElong1() {
+        return elong1;
+    }
+
+    public void setElong1(String elong1) {
+        this.elong1 = elong1;
+    }
+
+    public String getElong2() {
+        return elong2;
+    }
+
+    public void setElong2(String elong2) {
+        this.elong2 = elong2;
+    }
+
     public String getHardness1() {
         return hardness1;
     }
@@ -491,6 +385,30 @@ public class searchallmaterialdata {
         this.hardness3 = hardness3;
     }
 
+    public String getImpactp1() {
+        return impactp1;
+    }
+
+    public void setImpactp1(String impactp1) {
+        this.impactp1 = impactp1;
+    }
+
+    public String getImpactp2() {
+        return impactp2;
+    }
+
+    public void setImpactp2(String impactp2) {
+        this.impactp2 = impactp2;
+    }
+
+    public String getImpactp3() {
+        return impactp3;
+    }
+
+    public void setImpactp3(String impactp3) {
+        this.impactp3 = impactp3;
+    }
+
     public String getBendangle() {
         return bendangle;
     }
@@ -508,12 +426,93 @@ public class searchallmaterialdata {
     }
 
 
+
     public String getHeatcondi() {
         return heatcondi;
     }
 
     public void setHeatcondi(String heatcondi) {
         this.heatcondi = heatcondi;
+    }
+
+    public String getAls() {
+        return als;
+    }
+
+    public void setAls(String als) {
+        this.als = als;
+    }
+
+    public String getFe() {
+        return fe;
+    }
+
+    public void setFe(String fe) {
+        this.fe = fe;
+    }
+
+    public String getZn() {
+        return zn;
+    }
+
+    public void setZn(String zn) {
+        this.zn = zn;
+    }
+
+    public String getB() {
+        return b;
+    }
+
+    public void setB(String b) {
+        this.b = b;
+    }
+
+    public String getW() {
+        return w;
+    }
+
+    public void setW(String w) {
+        this.w = w;
+    }
+
+    public String getSb() {
+        return sb;
+    }
+
+    public void setSb(String sb) {
+        this.sb = sb;
+    }
+
+    public String getAl() {
+        return al;
+    }
+
+    public void setAl(String al) {
+        this.al = al;
+    }
+
+    public String getZr() {
+        return zr;
+    }
+
+    public void setZr(String zr) {
+        this.zr = zr;
+    }
+
+    public String getCa() {
+        return ca;
+    }
+
+    public void setCa(String ca) {
+        this.ca = ca;
+    }
+
+    public String getBe() {
+        return be;
+    }
+
+    public void setBe(String be) {
+        this.be = be;
     }
 
     public int getUtclass() {
