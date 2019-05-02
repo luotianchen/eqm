64a795ccb1dc011b954502ff48ffb9d6cac045f5
ousc <750154432@qq.com>
2019-05-02 16:15:05 +0800

    update

--- front/src/app/core/api/api.service.ts
+++ front/src/app/core/api/api.service.ts
@@ -5,6 +5,6 @@ import {Injectable} from "@angular/core";
 
 @Injectable()
 export class ApiService{
-  // public BASEURL = "http://localhost:8080";
-  public BASEURL = "/eqm";
+  public BASEURL = "http://localhost:8080";
+  // public BASEURL = "/eqm";
 }
